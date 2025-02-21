7298c898266e4bc91b4feab656a97bbcad4cad52
Artem Agvanian <artemagvanian@icloud.com>
2025-02-21 15:31:49 -0500

    Complete tests for M4

--- test/test-student/m4.student.test.js
+++ test/test-student/m4.student.test.js
@@ -7,30 +7,180 @@
 */
 
 const distribution = require('../../config.js');
+const id = distribution.util.id;
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  const user = {foo: 'bar'};
+  const key = 'foobar';
+
+  distribution.local.mem.put(user, key, (e, v) => {
+    distribution.local.mem.del(key, (e, v) => {
+      distribution.local.mem.del(key, (e, v) => {
+        try {
+          expect(e).toBeInstanceOf(Error);
+          expect(v).toBeFalsy();
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+  });
 });
 
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  const user = {foo: 'bar'};
+  const key = 'foobar';
+
+  distribution.mygroup.mem.put(user, key, (e, v) => {
+    distribution.local.mem.get(key, (e, v) => {
+      try {
+        expect(e).toBeInstanceOf(Error);
+        expect(v).toBeFalsy();
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
 });
 
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  const user = {foo: 'bar'};
+  const key = 'foobar';
+
+  distribution.local.store.put(user, key, (e, v) => {
+    distribution.local.store.del(key, (e, v) => {
+      distribution.local.store.del(key, (e, v) => {
+        try {
+          expect(e).toBeInstanceOf(Error);
+          expect(v).toBeFalsy();
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+  });
 });
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  const user = {foo: 'bar'};
+  const key = 'foobar';
+
+  distribution.mygroup.store.put(user, key, (e, v) => {
+    distribution.local.store.get(key, (e, v) => {
+      try {
+        expect(e).toBeInstanceOf(Error);
+        expect(v).toBeFalsy();
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
 });
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  const user = {foo: 'bar'};
+  const key = 'foobar';
+
+  distribution.local.store.put(user, key, (e, v) => {
+    user.foo = 'foo';
+    distribution.local.store.put(user, key, (e, v) => {
+      distribution.local.store.get(key, (e, v) => {
+        try {
+          expect(e).toBeFalsy();
+          expect(v).toEqual(user);
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+  });
 });
+
+// SETUP CODE ADAPTED FROM STENCIL BELOW:
+
+/*
+  Testing infrastructure code.
+*/
+
+// This group is used for testing most of the functionality
+const mygroupGroup = {};
+
+/*
+   This hack is necessary since we can not
+   gracefully stop the local listening node.
+   This is because the process that node is
+   running in is the actual jest process
+*/
+let localServer = null;
+
+const n1 = {ip: '127.0.0.1', port: 8000};
+const n2 = {ip: '127.0.0.1', port: 8001};
+const n3 = {ip: '127.0.0.1', port: 8002};
+
+
+beforeAll((done) => {
+  // First, stop the nodes if they are running
+  const remote = {service: 'status', method: 'stop'};
+
+  remote.node = n1;
+  distribution.local.comm.send([], remote, (e, v) => {
+    remote.node = n2;
+    distribution.local.comm.send([], remote, (e, v) => {
+      remote.node = n3;
+      distribution.local.comm.send([], remote, (e, v) => {
+      });
+    });
+  });
+
+  mygroupGroup[id.getSID(n1)] = n1;
+  mygroupGroup[id.getSID(n2)] = n2;
+  mygroupGroup[id.getSID(n3)] = n3;
+
+  // Now, start the base listening node
+  distribution.node.start((server) => {
+    localServer = server;
+
+    const groupInstantiation = (e, v) => {
+      const mygroupConfig = {gid: 'mygroup'};
+
+      // Create some groups
+      distribution.local.groups
+          .put(mygroupConfig, mygroupGroup, (e, v) => {
+            done();
+          });
+    };
+
+    // Start the nodes
+    distribution.local.status.spawn(n1, (e, v) => {
+      distribution.local.status.spawn(n2, (e, v) => {
+        distribution.local.status.spawn(n3, groupInstantiation);
+      });
+    });
+  });
+});
+
+afterAll((done) => {
+  distribution.mygroup.status.stop((e, v) => {
+    const remote = {service: 'status', method: 'stop'};
+    remote.node = n1;
+    distribution.local.comm.send([], remote, (e, v) => {
+      remote.node = n2;
+      distribution.local.comm.send([], remote, (e, v) => {
+        remote.node = n3;
+        distribution.local.comm.send([], remote, (e, v) => {
+          localServer.close();
+          done();
+        });
+      });
+    });
+  });
+});
+
+// SETUP CODE ADAPTED FROM STENCIL ABOVE
