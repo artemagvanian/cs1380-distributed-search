c51bf1510586a7687c5667b6a66abd5ee3e17923
Artem Agvanian <artemagvanian@icloud.com>
2025-03-16 14:21:26 -0400

    Complete tests for M5

--- test/test-student/m5.student.test.js
+++ test/test-student/m5.student.test.js
@@ -8,29 +8,304 @@
 
 const distribution = require('../../config.js');
 
+const id = distribution.util.id;
+
+const cfreqGroup = {};
+
+let localServer = null;
+
+const n1 = {ip: '127.0.0.1', port: 7110};
+const n2 = {ip: '127.0.0.1', port: 7111};
+const n3 = {ip: '127.0.0.1', port: 7112};
+
+/* The tests below are variations on the original cfreq test provided by the course staff */
+
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  // Calculate the frequency of each character in a set of documents
+  const mapper = (key, value) => {
+    const chars = value.replace(/\s+/g, '').split('');
+    const out = [];
+    chars.forEach((char) => {
+      const o = {};
+      o[char] = 1;
+      out.push(o);
+    });
+    return out;
+  };
+
+  const reducer = (key, values) => {
+    const out = {};
+    out[key] = values.reduce((sum, v) => sum + v, 0);
+    return out;
+  };
+
+  const dataset = [
+    {'doc1': 'hello world'},
+    {'doc2': 'map reduce test'},
+    {'doc3': 'character counting example'},
+    {'doc4': ''},
+    {'doc5': ''},
+  ];
+
+  const expected = [
+    {'h': 2}, {'e': 7}, {'l': 4},
+    {'o': 3}, {'w': 1}, {'r': 4},
+    {'d': 2}, {'m': 2}, {'a': 4},
+    {'p': 2}, {'u': 2}, {'c': 4},
+    {'t': 4}, {'s': 1}, {'n': 2},
+    {'i': 1}, {'g': 1}, {'x': 1},
+  ];
+
+  const doMapReduce = (cb) => {
+    distribution.cfreq.mr.exec({keys: dataset.map((e) => Object.keys(e)[0]), map: mapper, reduce: reducer}, (e, v) => {
+      try {
+        expect(v).toEqual(expect.arrayContaining(expected));
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  };
+
+  let cntr = 0;
+
+  dataset.forEach((o) => {
+    const key = Object.keys(o)[0];
+    const value = o[key];
+    distribution.cfreq.store.put(value, key, (e, v) => {
+      cntr++;
+      if (cntr === dataset.length) {
+        doMapReduce();
+      }
+    });
+  });
 });
 
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  // Calculate the frequency of each character in a set of documents
+  const mapper = (key, value) => {
+    const chars = value.replace(/\s+/g, '').split('');
+    const out = [];
+    chars.forEach((char) => {
+      const o = {};
+      o[char] = 1;
+      out.push(o);
+    });
+    return out;
+  };
+
+  const reducer = (key, values) => {
+    const out = {};
+    out[key] = values.reduce((sum, v) => sum + v, 0);
+    return out;
+  };
+
+  const dataset = [{'doc6': ''}];
+
+  const expected = [];
+
+  const doMapReduce = (cb) => {
+    distribution.cfreq.mr.exec({keys: dataset.map((e) => Object.keys(e)[0]), map: mapper, reduce: reducer}, (e, v) => {
+      try {
+        expect(v).toEqual(expect.arrayContaining(expected));
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  };
+
+  let cntr = 0;
+
+  dataset.forEach((o) => {
+    const key = Object.keys(o)[0];
+    const value = o[key];
+    distribution.cfreq.store.put(value, key, (e, v) => {
+      cntr++;
+      if (cntr === dataset.length) {
+        doMapReduce();
+      }
+    });
+  });
 });
 
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  // Calculate the frequency of each character in a set of documents
+  const mapper = (key, value) => {
+    const chars = value.replace(/\s+/g, '').split('');
+    const out = [];
+    chars.forEach((char) => {
+      const o = {};
+      o[char] = 1;
+      out.push(o);
+    });
+    return out;
+  };
+
+  const reducer = (key, values) => {
+    const out = {};
+    out[key] = values.reduce((sum, v) => sum + v, 0);
+    return out;
+  };
+
+  const dataset = [];
+
+  const expected = [];
+
+  const doMapReduce = (cb) => {
+    distribution.cfreq.mr.exec({keys: dataset.map((e) => Object.keys(e)[0]), map: mapper, reduce: reducer}, (e, v) => {
+      try {
+        expect(v).toEqual(expect.arrayContaining(expected));
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  };
+
+  doMapReduce();
 });
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  // Calculate the frequency of each character in a set of documents
+  const mapper = (key, value) => {
+    const chars = value.replace(/\s+/g, '').split('');
+    const out = [];
+    chars.forEach((char) => {
+      const o = {};
+      o[char] = 1;
+      out.push(o);
+    });
+    return out;
+  };
+
+  const reducer = (key, values) => {
+    const out = {};
+    out[key] = values.reduce((sum, v) => sum + v, 0);
+    return out;
+  };
+
+  const dataset = [{'doc7': 'aaaaa'}, {'doc8': 'aaaaa'}, {'doc9': 'aaaaa'}, {'doc10': 'aaaaa'}, {'doc11': 'aaaaa'}];
+
+  const expected = [{'a': 25}];
+
+  const doMapReduce = (cb) => {
+    distribution.cfreq.mr.exec({keys: dataset.map((e) => Object.keys(e)[0]), map: mapper, reduce: reducer}, (e, v) => {
+      try {
+        expect(v).toEqual(expect.arrayContaining(expected));
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  };
+
+  let cntr = 0;
+
+  dataset.forEach((o) => {
+    const key = Object.keys(o)[0];
+    const value = o[key];
+    distribution.cfreq.store.put(value, key, (e, v) => {
+      cntr++;
+      if (cntr === dataset.length) {
+        doMapReduce();
+      }
+    });
+  });
 });
 
 test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  // Calculate the frequency of each character in a set of documents
+  const mapper = (key, value) => {
+    const chars = value.replace(/\s+/g, '').split('');
+    const out = [];
+    chars.forEach((char) => {
+      const o = {};
+      o[char] = 1;
+      out.push(o);
+    });
+    return out;
+  };
+
+  const reducer = (key, values) => {
+    const out = {};
+    out[key] = values.reduce((sum, v) => sum + v, 0);
+    return out;
+  };
+
+  const dataset = [{'doc12': 'aaaaa'}, {'doc13': 'AAAAA'}, {'doc14': 'aAaAa'}, {'doc15': 'AaAaA'}];
+
+  const expected = [{'a': 10}, {'A': 10}];
+
+  const doMapReduce = (cb) => {
+    distribution.cfreq.mr.exec({keys: dataset.map((e) => Object.keys(e)[0]), map: mapper, reduce: reducer}, (e, v) => {
+      try {
+        expect(v).toEqual(expect.arrayContaining(expected));
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  };
+
+  let cntr = 0;
+
+  dataset.forEach((o) => {
+    const key = Object.keys(o)[0];
+    const value = o[key];
+    distribution.cfreq.store.put(value, key, (e, v) => {
+      cntr++;
+      if (cntr === dataset.length) {
+        doMapReduce();
+      }
+    });
+  });
+});
+
+beforeAll((done) => {
+  cfreqGroup[id.getSID(n1)] = n1;
+  cfreqGroup[id.getSID(n2)] = n2;
+  cfreqGroup[id.getSID(n3)] = n3;
+
+
+  const startNodes = (cb) => {
+    distribution.local.status.spawn(n1, (e, v) => {
+      distribution.local.status.spawn(n2, (e, v) => {
+        distribution.local.status.spawn(n3, (e, v) => {
+          cb();
+        });
+      });
+    });
+  };
+
+  distribution.node.start((server) => {
+    localServer = server;
+
+    startNodes(() => {
+      const cfreqConfig = {gid: 'cfreq'};
+      distribution.local.groups.put(cfreqConfig, cfreqGroup, (e, v) => {
+        distribution.cfreq.groups.put(cfreqConfig, cfreqGroup, (e, v) => {
+          done();
+        });
+      });
+    });
+  });
+});
+
+
+afterAll((done) => {
+  const remote = {service: 'status', method: 'stop'};
+  remote.node = n1;
+  distribution.local.comm.send([], remote, (e, v) => {
+    remote.node = n2;
+    distribution.local.comm.send([], remote, (e, v) => {
+      remote.node = n3;
+      distribution.local.comm.send([], remote, (e, v) => {
+        localServer.close();
+        done();
+      });
+    });
+  });
 });
